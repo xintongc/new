99352b1b6f61a0d992829d6e8c6a883f0cf3bacf
Xintong Cheng <xintongc@gmail.com>
2019-02-14 15:12:13 -0500

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,25 @@
+An API that returns a Promise will result in a promise chain, and it splits the function into many parts. Consider the following code:
+
+function getProcessedData(url) {
+  return downloadData(url) // returns a promise
+    .catch(e => {
+      return downloadFallbackData(url)  // returns a promise
+    })
+    .then(v => {
+      return processDataInWorker(v); // returns a promise
+    });
+}
+
+
+
+it can be rewritten with a single async function as follows:
+
+async function getProcessedData(url) {
+  let v;
+  try {
+    v = await downloadData(url); 
+  } catch(e) {
+    v = await downloadFallbackData(url);
+  }
+  return processDataInWorker(v);
+}
